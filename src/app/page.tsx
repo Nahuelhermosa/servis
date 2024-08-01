93c8791fe40687569d762631eb83675cc62773db
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2024-07-31 23:32:14 -0300

    ultimo  arreglo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://porfolio-swart-eight.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
